4cfc4e38598713884148776ca6785462482e40ce
steven20406 <steven20406@gmail.com>
2017-11-07 22:08:42 +0800

    Edit insert

--- DBControl/insert_note.php
+++ DBControl/insert_note.php
@@ -5,5 +5,12 @@
  * Date: 2017/11/7
  * Time: 下午 09:21
  */
-echo $_POST["user_name"];
-echo $_POST["user_note"];
+include_once ("dbtool.inc.php");
+
+$link=create_connection();
+
+$name = $_POST["user_name"];
+$note = $_POST["user_note"];
+
+$query="insert into note_board values ('$name', '$note')";
+$result = execute_query($link,"test", $query);
