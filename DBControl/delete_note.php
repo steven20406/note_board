fadc740327e1965184889dea1535c395d696c2c5
steven20406 <steven20406@gmail.com>
2017-11-08 11:38:50 +0800

    Update delete_note.php

--- DBControl/delete_note.php
+++ DBControl/delete_note.php
@@ -2,3 +2,6 @@
 
 include_once ("dbtool.inc.php");
 $link=create_connection();
+$delete_note = $_POST["delete"];
+$query="delete from user_note where user_note = $delete_note";
+$result=execute_query($link,"note_board",$query);
